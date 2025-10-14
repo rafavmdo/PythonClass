5ad96cbd6d5d9dbe0fc72096f0f051536a5a5062
rafinha <rafaelamarostegan@gmail.com>
2025-10-14 15:50:53 -0300

    Update chefaofinal.py

--- rafa_python/dia 6/chefaofinal.py
+++ rafa_python/dia 6/chefaofinal.py
@@ -9,15 +9,16 @@ jogada_computador = ["pedra", "papel", "tesoura"]
 decisao = random.randrange (jogada_computador)
 jogada_usuario = input("Sua jogada (pedra, papel, tesoura): ").lower()
 
-print(f"\nO computador escolheu: {jogada_computador}")
+print(f"\nO computador escolheu: {decisao}")
 print(f"Você escolheu: {jogada_usuario}")
 
 # Lógica do jogo
 if jogada_usuario == decisao:
     print("Resultado: Empate!")
-elif (jogada_usuario == "pedra" and jogada_computador == "tesoura") or \
-     (jogada_usuario == "tesoura" and jogada_computador == "papel") or \
-     (jogada_usuario == "papel" and jogada_computador == "pedra"):
+elif (jogada_usuario == "pedra" and decisao == "tesoura") or \
+     (jogada_usuario == "tesoura" and decisao == "papel") or \
+     (jogada_usuario == "papel" and decisao == "pedra"):
     print("Resultado: Você venceu! \U0001F3C6")
 else:
+
     print("Resultado: O computador venceu! \U0001F916")
